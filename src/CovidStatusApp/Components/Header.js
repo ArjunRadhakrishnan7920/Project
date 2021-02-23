034f1ab1327b2fdf87abea7ee587a6a1e92c9d14
ArjunRadhakrishnan7920 <arjunrillathu@gmail.com>
2021-02-23 22:56:01 +0530

    covid

---
+++ src/CovidStatusApp/Components/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+ import {Navbar,Nav} from "react-bootstrap"
+ import{Link}from "react-router-dom"
+
+class Header extends Component {
+    render() {
+        return (
+            
+ <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand >COVID-19 </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                
+
+                    <Link className="nav-link" to="/india">India</Link>
+                    <Link className="nav-link" to="/world">World</Link>
+                    
+                </Nav>
+    
+    
+  </Navbar.Collapse>
+</Navbar>
+            
+        )
+    }
+}
+
+export default Header
